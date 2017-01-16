351b2dfb25b202925e2a997eaaa436756a42af7b
Ivan ZS <ivan@pokutan.com>
2017-01-16 20:55:17 +0200

    fix batch

--- EnglishTraining/copy.bat
+++ EnglishTraining/copy.bat
@@ -2,4 +2,4 @@ REM ROBOCOPY $(OutputPath) $(HOME_DROPBOX)eng\my_app\ $(TargetFileName) /L /V
 REM %1 - $(OutputPath)
 REM %2 - $(HOME_DROPBOX)eng\my_app\
 REM %3 - $(TargetFileName)
-ROBOCOPY %1 %2 %3 /L /V
+ROBOCOPY %1 %2 %3 /V
